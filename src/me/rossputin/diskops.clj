4d9287132c18cac98e795b582c01a18ceaa803f1
rossputin <rossajmcd@gmail.com>
2013-08-08 00:55:59 +0100

    add shallow copy for simple file copying, no mkdirs

--- src/me/rossputin/diskops.clj
+++ src/me/rossputin/diskops.clj
@@ -6,9 +6,26 @@
   [d]
   (doseq [f (reverse (file-seq (file d)))] (delete-file f)))
 
-(defn copy-recursive
-  [src dest]
-  (doseq [f (remove #(.isDirectory %) (file-seq (file src)))]
+(defn recursive-copy-input
+  [src]
+  (remove #(.isDirectory %) (file-seq (file src))))
+
+(defn perform-copy
+  [f-seq dest]
+  (doseq [f f-seq]
     (let [dest-file (file dest f)]
       (.mkdirs (.getParentFile dest-file))
       (copy f dest-file))))
+
+(defn copy-recursive
+  [src dest]
+  (perform-copy (recursive-copy-input src) dest))
+
+(defn perform-shallow-copy
+  [f-seq dest]
+  (doseq [f f-seq] (copy f (file dest (.getName f)))))
+
+(defn copy-recursive-children
+  [src dest]
+  (let [c-set (rest (recursive-copy-input src))]
+    (perform-shallow-copy c-set dest)))
