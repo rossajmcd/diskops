388a200da18333515a6787214756baad32e8e2a7
rossputin <rossajmcd@gmail.com>
2013-08-08 22:54:15 +0100

    fix non recursive copy, refactor to move some functions from silk

--- src/me/rossputin/diskops.clj
+++ src/me/rossputin/diskops.clj
@@ -2,13 +2,9 @@
   (:require [clojure.java.io :refer [copy delete-file file]])
   (import java.io.File))
 
-(defn delete-directory
-  [d]
-  (doseq [f (reverse (file-seq (file d)))] (delete-file f)))
-
-(defn recursive-copy-input
-  [src]
-  (remove #(.isDirectory %) (file-seq (file src))))
+;; =============================================================================
+;; Helper functions
+;; =============================================================================
 
 (defn perform-copy
   [f-seq dest]
@@ -17,15 +13,51 @@
       (.mkdirs (.getParentFile dest-file))
       (copy f dest-file))))
 
-(defn copy-recursive
-  [src dest]
-  (perform-copy (recursive-copy-input src) dest))
-
 (defn perform-shallow-copy
   [f-seq dest]
   (doseq [f f-seq] (copy f (file dest (.getName f)))))
 
-(defn copy-recursive-children
+
+;; =============================================================================
+;; Disk based IO operations
+;; =============================================================================
+
+(defn path-list
+  [p]
+  (.listFiles (file p)))
+
+(defn recursive-list
+  [src]
+  (remove #(.isDirectory %) (file-seq (file src))))
+
+(defn file?
+  [p]
+  (.isFile (file p)))
+
+(defn dir?
+  [p]
+  (.isDirectory (file p)))
+
+(defn files-list
+  [p]
+  (filter #(file? %) (path-list p)))
+
+(defn exists?
+  [p]
+  (.exists (file p)))
+
+(defn exists-dir?
+  [p]
+  (and (exists? p) (dir? p)))
+
+(defn copy-recursive
   [src dest]
-  (let [c-set (rest (recursive-copy-input src))]
-    (perform-shallow-copy c-set dest)))
+  (perform-copy (recursive-list src) dest))
+
+(defn copy-file-children
+  [src dest]
+  (perform-shallow-copy (files-list src) dest))
+
+(defn delete-directory
+  [d]
+  (doseq [f (reverse (file-seq (file d)))] (delete-file f)))
