901f5d5c7aff3c22c71ca0f0c1a16b290099a1f3
rossputin <rossajmcd@gmail.com>
2017-05-27 09:39:52 +0100

    add path functionality and remove those annoying java-isms

--- src/me/rossputin/diskops.clj
+++ src/me/rossputin/diskops.clj
@@ -1,6 +1,7 @@
 (ns me.rossputin.diskops
   (:require [clojure.java.io :refer [as-relative-path copy delete-file file]]
-            [clojure.string :as s])
+            [clojure.string :refer [join split]])
+  (:refer-clojure :exclude [name])
   (:import java.io.File))
 
 ;; =============================================================================
@@ -23,6 +24,16 @@
 
 (defn dir? [p] (when (.isDirectory (file p)) p))
 
+(defn parent [f] (.getParent f))
+
+(defn parentFile [f] (.getParentFile f))
+
+(defn path [f] (.getPath f))
+
+(defn name [f] (.getName f))
+
+(defn parentPath [f] (path (parentFile f)))
+
 (defn paths [p] (.listFiles (file p)))
 
 (defn files [p] (filter #(file? %) (paths p)))
@@ -35,12 +46,16 @@
 
 (defn exists-dir? [p] (when (and (exists? p) (dir? p)) p))
 
+(defn mkdir [f] (.mkdir f))
+
+(defn mkdirs [f] (.mkdirs f))
+
 (defn copy-recursive [src dest]
-  (let [parent (.getParent (file src))
+  (let [parent (parent (file src))
         idx (if (nil? parent) 0 (.length parent))]
     (doseq [f (files-recursive src)]
-      (let [dest-file (file (str dest (fs) (subs (.getPath f) idx)))]
-        (.mkdirs (.getParentFile dest-file))
+      (let [dest-file (file (str dest (fs) (subs (path f) idx)))]
+        (mkdirs (parentFile dest-file))
         (copy f dest-file)))))
 
 (defn copy-file-children [src dest] (shallow-copy (files src) dest))
@@ -49,10 +64,10 @@
   (doseq [f (reverse (file-seq (file d)))] (delete-file f)))
 
 (defn has-ext? [file exts]
-  (let [ext-pattern (s/join "|" exts)
+  (let [ext-pattern (join "|" exts)
         complete-pattern (str "^.+\\.(" ext-pattern ")$")
         exts-reg-exp (re-pattern complete-pattern)]
-    (when (re-find exts-reg-exp (.getName file)) file)))
+    (when (re-find exts-reg-exp (name file)) file)))
 
 (defn filter-exts [files exts] (filter #(has-ext? % exts) files))
 
@@ -64,6 +79,6 @@
 
 (defn as-relative [path] (try (as-relative-path path) (catch Exception e nil)))
 
-(defn filename [f] (if f (first (s/split (.getName f) #"\.")) nil))
+(defn filename [f] (if f (first (split (name f) #"\.")) nil))
 
-(defn extension [f] (if f (last (s/split (.getName f) #"\.")) nil))
+(defn extension [f] (if f (last (split (name f) #"\.")) nil))
