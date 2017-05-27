1e6b2b3905c02b4d2c055f3023009f1f4b1c8a9d
rossputin <rossajmcd@gmail.com>
2017-05-27 09:39:28 +0100

    update clojure dependency and bump version to 0.7.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject me.rossputin/diskops "0.6.0"
+(defproject me.rossputin/diskops "0.7.0"
   :description "Just a handful of the simplest of ubiquitous disk operations."
   :url "https://github.com/rossputin/diskops"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
 
   :aot :all)
